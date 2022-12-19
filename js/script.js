0f4327c78ab77e19ed834aba413daf82217f990b
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-19 21:54:16 +0100

    fixing issue about bombs array

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-/*
-Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco.
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-In seguito l'utente clicca su una cella:
-se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
-Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti
-(ovvero quando ha rivelato tutte le celle che non sono bombe).
-Al termine della partita il software deve comunicare il punteggio,
-cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-*/
-
 //* FUNCTIONS
 const cellGenerator = (number) => {
     const cell = document.createElement('div');
@@ -35,7 +22,6 @@ const h2Element = document.querySelector('h2');
 const targetElement = document.getElementById('target');
 //* OTHERS KIND OF VARIABLES
 let score = 0; // variable to count user score
-const bombsArrey = [];
 let message;
 
 
@@ -47,13 +33,14 @@ playButtonElement.addEventListener('click', function(){
     score = 0; // starting point each time is clicked on play
     targetElement.innerHTML = ''; // needed to remove message by clicking play again
     gridElement.innerHTML = ''; // needed to remove colored cells by pressing button again
+    const bombsArray = []; //as name said: vecotr with bombs position
 
     //* 'BOMBS' GENERATOR
     for (let i = 0; i < 16; i++){
-        const random = getRandomNumber(1,100,bombsArrey);
-        bombsArrey.push(random);
+        const random = getRandomNumber(1,100,bombsArray);
+        bombsArray.push(random);
     }
-    console.log(bombsArrey);
+    console.log(bombsArray); // console log to see where the bombs are
 
 
     //* GRID'S CELLS
@@ -71,7 +58,7 @@ playButtonElement.addEventListener('click', function(){
                 return; // if cell contains class 'clicked' function gets stopped so there won't be incremental score
             }
             
-            if (bombsArrey.includes(parseInt(cell.textContent))) {
+            if (bombsArray.includes(parseInt(cell.textContent))) {
                 cell.classList.add('bomb');
                 message = `hai preso una bomba, hai perso :( <br> hai totalizzato ${score} punti`;
                 targetElement.innerText = message;
@@ -87,7 +74,6 @@ playButtonElement.addEventListener('click', function(){
                     }
             }
             targetElement.innerHTML = message;
-            console.log(bombsArrey)
         })
         
         gridElement.appendChild(cell); // rendering cells on page
